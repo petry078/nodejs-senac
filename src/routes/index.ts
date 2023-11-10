da325e574118e83de8c487c1d011d91a20c5728c
GUILHERME PETRY <guilherme.petry@senacsp.edu.br>
2023-11-10 17:16:05 -0300

    aula3

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,14 +3,30 @@ import {Router, Request, Response} from 'express'
 const router = Router()
 
 router.get('/', (req:Request , res:Response) => {
-    res.send("/")
+    let idade:number = 26
+    let mostrarIdade:boolean = false
+
+    if(idade > 18){
+        mostrarIdade = true
+    }
+
+    res.render('home', {
+        name: 'Guilherme',
+        mostrarIdade,
+        produtos: [
+            {nomeDoProduto: 'Arroz', preco: 14.99},
+            {nomeDoProduto: 'Feijão', preco: 6.49},
+            {nomeDoProduto: 'Brócolis', preco: 4.12}   
+        ]
+    })
+
 })
 
 router.get('/contato', (req:Request , res:Response) => {
-    res.send("/contato")
+    res.render('contato')
 })
 
-//rota dinamica
+//rotas dinamicas
 router.get("/noticias/:slug", (req:Request , res:Response) => {
     let slug:string = req.params.slug
     res.send(`Notícia: ${slug}`)
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
 import express, {Request, Response} from 'express'
 import mainRoutes from './routes/index'
+import path from 'path'
+import mustache from 'mustache-express'
 
 //atribui express() para a variável servidor
-const servidor = express()
+const server = express()
 
-servidor.use(mainRoutes)
+//atribui mustache() para a variavel mustache
+server.set('view engine', 'mustache')
+
+//atribui o caminho da pasta views (html)
+server.set('views',path.join(__dirname,'views'))
+server.engine('mustache',mustache())
+
+//atribui o caminho da pasta public (imagens e css)
+server.use(express.static(path.join(__dirname, '../public')))
+
+server.use(mainRoutes)
+
+//404 page
+server.use((req:Request, res:Response) => {
+   res.status(404).send("Erro 404. Página não encontrada") 
+})
 
 //express.ouvindo a porta 3000
-servidor.listen(3000)
+server.listen(3000)
 console.log(`http://localhost:3000/`)
